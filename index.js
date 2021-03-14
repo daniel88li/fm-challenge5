141666f1c977df1d8af02bc0488fd99ea1878dcd
Daniel Li <daniel88_li@hotmail.com>
2021-03-14 16:04:51 -0400

    Added error styles

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 "use strict";
 
-const submitBtn = document.querySelector(".submit-btn");
+const form = document.querySelector("form");
+const inputList = document.querySelectorAll("label > input");
 
-submitBtn.addEventListener("click", function (e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
+  for (let eachInput of inputList) {
+    let errorBox = eachInput.nextElementSibling;
+    let errorIcon = eachInput.previousElementSibling;
+    let eachValue = eachInput.value.trim();
+
+    if (eachInput.validity.valueMissing || eachValue === "") {
+      eachInput.setCustomValidity("Empty field");
+      errorBox.textContent = `${eachInput.placeholder} cannot be empty`;
+      errorIcon.style.display = "block";
+    } else {
+      eachInput.setCustomValidity("");
+      errorBox.textContent = "";
+      errorIcon.style.display = "none";
+    }
+
+    if (eachInput.name === "email") {
+      if (eachInput.validity.typeMismatch) {
+        errorBox.textContent = "Looks like this is not an email";
+        errorIcon.style.display = "block";
+      } else {
+        errorBox.textContent = errorBox.textContent ? errorBox.textContent : "";
+        errorIcon.style.display =
+          errorIcon.style.display === "none" ? "none" : "block";
+      }
+    }
+  }
 });
